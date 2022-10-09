1473edf6bef21af0fd86b4b5377f91529a2d78ae
JiangQiang <jq1257403419@vip.qq.com>
2022-10-09 11:39:38 +0800

    mimetype类型映射

--- J-crawler-core/src/main/java/top/jiangqiang/core/util/FileUtil.java
+++ J-crawler-core/src/main/java/top/jiangqiang/core/util/FileUtil.java
@@ -1,9 +1,11 @@
 package top.jiangqiang.core.util;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.HexUtil;
 import cn.hutool.core.util.RandomUtil;
@@ -22,6 +24,22 @@ import java.util.*;
  * @date 2022-09-30
  */
 public class FileUtil extends cn.hutool.core.io.FileUtil {
+    public final static Map<String, String> extNameMimeType;
+    public final static Map<String, List<String>> mimeTypeExtName;
+
+    static {
+        String jsonStr = ResourceUtil.readUtf8Str("mimeType.json");
+        extNameMimeType = JSONUtil.parse(jsonStr, new HashMap<>());
+        mimeTypeExtName = new HashMap<>();
+        extNameMimeType.forEach((key, value) -> {
+            List<String> valueList = mimeTypeExtName.get(value);
+            if (valueList == null) {
+                valueList = new LinkedList<>();
+            }
+            valueList.add(key);
+            mimeTypeExtName.put(value, valueList);
+        });
+    }
 
     /**
      * 将两个字符串按照一定规则混合，防止信息泄露
@@ -73,223 +91,22 @@ public class FileUtil extends cn.hutool.core.io.FileUtil {
     }
 
     private static String getTypeFromMimeType(String contentType) {
-        String str = """
-                'apk'        'application/vnd.android.package-archive',
-                '3gp'        'video/3gpp'
-                'ai'        'application/postscript'
-                'aif'        'audio/x-aiff'
-                'aifc'        'audio/x-aiff'
-                'aiff'        'audio/x-aiff'
-                'asc'        'text/plain'
-                'atom'        'application/atom+xml'
-                'au'        'audio/basic'
-                'avi'        'video/x-msvideo'
-                'bcpio'        'application/x-bcpio'
-                'bin'        'application/octet-stream'
-                'bmp'        'image/bmp'
-                'cdf'        'application/x-netcdf'
-                'cgm'        'image/cgm'
-                'class'        'application/octet-stream'
-                'cpio'        'application/x-cpio'
-                'cpt'        'application/mac-compactpro'
-                'csh'        'application/x-csh'
-                'css'        'text/css'
-                'dcr'        'application/x-director'
-                'dif'        'video/x-dv'
-                'dir'        'application/x-director'
-                'djv'        'image/vnd.djvu'
-                'djvu'        'image/vnd.djvu'
-                'dll'        'application/octet-stream'
-                'dmg'        'application/octet-stream'
-                'dms'        'application/octet-stream'
-                'doc'        'application/msword'
-                'dtd'        'application/xml-dtd'
-                'dv'        'video/x-dv'
-                'dvi'        'application/x-dvi'
-                'dxr'        'application/x-director'
-                'eps'        'application/postscript'
-                'etx'        'text/x-setext'
-                'exe'        'application/octet-stream'
-                'ez'        'application/andrew-inset'
-                'flv'        'video/x-flv'
-                'gif'        'image/gif'
-                'gram'        'application/srgs'
-                'grxml'        'application/srgs+xml'
-                'gtar'        'application/x-gtar'
-                'gz'        'application/x-gzip'
-                'hdf'        'application/x-hdf'
-                'hqx'        'application/mac-binhex40'
-                'htm'        'text/html'
-                'html'        'text/html'
-                'ice'        'x-conference/x-cooltalk'
-                'ico'        'image/x-icon'
-                'ics'        'text/calendar'
-                'ief'        'image/ief'
-                'ifb'        'text/calendar'
-                'iges'        'model/iges'
-                'igs'        'model/iges'
-                'jnlp'        'application/x-java-jnlp-file'
-                'jp2'        'image/jp2'
-                'jpe'        'image/jpeg'
-                'jpeg'        'image/jpeg'
-                'jpg'        'image/jpeg'
-                'js'        'application/x-javascript'
-                'kar'        'audio/midi'
-                'latex'        'application/x-latex'
-                'lha'        'application/octet-stream'
-                'lzh'        'application/octet-stream'
-                'm3u'        'audio/x-mpegurl'
-                'm4a'        'audio/mp4a-latm'
-                'm4p'        'audio/mp4a-latm'
-                'm4u'        'video/vnd.mpegurl'
-                'm4v'        'video/x-m4v'
-                'mac'        'image/x-macpaint'
-                'man'        'application/x-troff-man'
-                'mathml'        'application/mathml+xml'
-                'me'        'application/x-troff-me'
-                'mesh'        'model/mesh'
-                'mid'        'audio/midi'
-                'midi'        'audio/midi'
-                'mif'        'application/vnd.mif'
-                'mov'        'video/quicktime'
-                'movie'        'video/x-sgi-movie'
-                'mp2'        'audio/mpeg'
-                'mp3'        'audio/mpeg'
-                'mp4'        'video/mp4'
-                'mpe'        'video/mpeg'
-                'mpeg'        'video/mpeg'
-                'mpg'        'video/mpeg'
-                'mpga'        'audio/mpeg'
-                'ms'        'application/x-troff-ms'
-                'msh'        'model/mesh'
-                'mxu'        'video/vnd.mpegurl'
-                'nc'        'application/x-netcdf'
-                'oda'        'application/oda'
-                'ogg'        'application/ogg'
-                'ogv'        'video/ogv'
-                'pbm'        'image/x-portable-bitmap'
-                'pct'        'image/pict'
-                'pdb'        'chemical/x-pdb'
-                'pdf'        'application/pdf'
-                'pgm'        'image/x-portable-graymap'
-                'pgn'        'application/x-chess-pgn'
-                'pic'        'image/pict'
-                'pict'        'image/pict'
-                'png'        'image/png'
-                'pnm'        'image/x-portable-anymap'
-                'pnt'        'image/x-macpaint'
-                'pntg'        'image/x-macpaint'
-                'ppm'        'image/x-portable-pixmap'
-                'ppt'        'application/vnd.ms-powerpoint'
-                'ps'        'application/postscript'
-                'qt'        'video/quicktime'
-                'qti'        'image/x-quicktime'
-                'qtif'        'image/x-quicktime'
-                'ra'        'audio/x-pn-realaudio'
-                'ram'        'audio/x-pn-realaudio'
-                'ras'        'image/x-cmu-raster'
-                'rdf'        'application/rdf+xml'
-                'rgb'        'image/x-rgb'
-                'rm'        'application/vnd.rn-realmedia'
-                'roff'        'application/x-troff'
-                'rtf'        'text/rtf'
-                'rtx'        'text/richtext'
-                'sgm'        'text/sgml'
-                'sgml'        'text/sgml'
-                'sh'        'application/x-sh'
-                'shar'        'application/x-shar'
-                'silo'        'model/mesh'
-                'sit'        'application/x-stuffit'
-                'skd'        'application/x-koan'
-                'skm'        'application/x-koan'
-                'skp'        'application/x-koan'
-                'skt'        'application/x-koan'
-                'smi'        'application/smil'
-                'smil'        'application/smil'
-                'snd'        'audio/basic'
-                'so'        'application/octet-stream'
-                'spl'        'application/x-futuresplash'
-                'src'        'application/x-wais-source'
-                'sv4cpio'        'application/x-sv4cpio'
-                'sv4crc'        'application/x-sv4crc'
-                'svg'        'image/svg+xml'
-                'swf'        'application/x-shockwave-flash'
-                't'        'application/x-troff'
-                'tar'        'application/x-tar'
-                'tcl'        'application/x-tcl'
-                'tex'        'application/x-tex'
-                'texi'        'application/x-texinfo'
-                'texinfo'        'application/x-texinfo'
-                'tif'        'image/tiff'
-                'tiff'        'image/tiff'
-                'tr'        'application/x-troff'
-                'tsv'        'text/tab-separated-values'
-                'txt'        'text/plain'
-                'ustar'        'application/x-ustar'
-                'vcd'        'application/x-cdlink'
-                'vrml'        'model/vrml'
-                'vxml'        'application/voicexml+xml'
-                'wav'        'audio/x-wav'
-                'wbmp'        'image/vnd.wap.wbmp'
-                'wbxml'        'application/vnd.wap.wbxml'
-                'webm'        'video/webm'
-                'wml'        'text/vnd.wap.wml'
-                'wmlc'        'application/vnd.wap.wmlc'
-                'wmls'        'text/vnd.wap.wmlscript'
-                'wmlsc'        'application/vnd.wap.wmlscriptc'
-                'wmv'        'video/x-ms-wmv'
-                'wrl'        'model/vrml'
-                'xbm'        'image/x-xbitmap'
-                'xht'        'application/xhtml+xml'
-                'xhtml'        'application/xhtml+xml'
-                'xls'        'application/vnd.ms-excel'
-                'xml'        'application/xml'
-                'xpm'        'image/x-xpixmap'
-                'xsl'        'application/xml'
-                'xslt'        'application/xslt+xml'
-                'xul'        'application/vnd.mozilla.xul+xml'
-                'xwd'        'image/x-xwindowdump'
-                'xyz'        'chemical/x-xyz'
-                'zip'        'application/zip'
-                """;
-        String[] split = str.split("\n");
-        List<String> strings = Arrays.stream(split).toList();
-        List<String> strings1 = strings.stream().map(s -> s.trim()).filter(StrUtil::isNotBlank).toList();
-        Map<String, String> map = new HashMap<>();
-        strings1.stream().forEach(s -> {
-            String[] split1 = s.split("\\s+");
-            if (split1.length == 2) {
-                String key = split1[0].trim();
-                String value = split1[1].trim();
-                if (key.startsWith("'")) {
-                    key = key.substring(1);
-                }
-                if (key.endsWith("'")) {
-                    key = key.substring(0, key.length() - 1);
-                }
-
-                if (value.startsWith("'")) {
-                    value = value.substring(1);
-                }
-                if (value.endsWith("'")) {
-                    value = value.substring(0, value.length() - 1);
-                }
-                if (StrUtil.isNotBlank(key) && StrUtil.isNotBlank(value)) {
-                    map.put(key, value);
-                }
-            }
-        });
-        System.out.println(JSONUtil.toJsonPrettyStr(map));
         if (StrUtil.isBlank(contentType)) {
             return null;
         } else {
+            contentType = contentType.toLowerCase();
+            if (contentType.endsWith(";charset=utf-8")) {
+                contentType = StrUtil.subBefore(contentType, ";charset=utf-8", true);
+            }
+            if ("application/octet-stream".equals(contentType)) {
+                return null;
+            }
+            List<String> strings = mimeTypeExtName.get(contentType);
+            if (CollUtil.isNotEmpty(strings)) {
+                return strings.get(0);
+            }
             return null;
         }
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(getTypeFromMimeType(""));
     }
 
     public static String saveFileFromPage(Page page, String type, String dirPath) {
@@ -332,9 +149,7 @@ public class FileUtil extends cn.hutool.core.io.FileUtil {
 
     public static String downloadFile(InputStream inputStream, String type, String dirPath) {
         BufferedOutputStream bufferedOutputStream = null;
-        try (
-                BufferedInputStream bufferedInputStream = IoUtil.toBuffered(inputStream);
-        ) {
+        try (BufferedInputStream bufferedInputStream = IoUtil.toBuffered(inputStream);) {
             String fileName = genFileName();
             byte[] buffer = new byte[4096];
             int len;
